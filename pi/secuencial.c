cb13193ddaf44d016237a0867d6748df5a5b32d5
Camilo Jose Cruz Rivera <cruz.camilo@correounivalle.edu.co>
2019-03-31 18:37:58 -0500

    ajuste

--- pi/secuencial.c
+++ pi/secuencial.c
@@ -12,7 +12,7 @@ int main(int argc, char** argv) {
     srand(time(NULL)); 
 
     for (i = 0; i < (INTERVAL * INTERVAL); i++) { 
-        rand_x = (double)((rand() % (INTERVAL + 1)) / INTERVAL); 
+        rand_x = (double)(rand() % (INTERVAL + 1)) / INTERVAL; 
         rand_y = (double)(rand() % (INTERVAL + 1)) / INTERVAL; 
 
         origin_dist = rand_x * rand_x + rand_y * rand_y; 
@@ -23,7 +23,7 @@ int main(int argc, char** argv) {
         
         square_points++; 
 
-        pi = (double)((4 * circle_points) / square_points);
+        pi = (double)(4 * circle_points) / square_points;
         printf("%0.15f %0.15f %d %d - %0.15f \n\n", rand_x, rand_y, circle_points, square_points, pi);
     }
 }
